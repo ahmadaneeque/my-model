0e3e02be17dfa8285eeca5fab17e239c55520792
ahmad <ahmadaneequekhalid@gmail.com>
2020-10-05 18:00:00 +0500

    reverted to single ques message

--- producer.py
+++ producer.py
@@ -12,16 +12,16 @@ credentials = pika.PlainCredentials(rabbit_user, rabbit_password)
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_host, port=rabbit_port, credentials=credentials))
 channel = connection.channel()
 
-# channel.queue_declare(queue='hello')  for single message consumer  will not be created for multiple consumers here
-channel.exchange_declare(exchange='logs', exchange_type='fanout')  # for multiple consumer of same message
+channel.queue_declare(queue='hello')  # for single message consumer  will not be created for multiple consumers here
+#channel.exchange_declare(exchange='logs', exchange_type='fanout')  # for multiple consumer of same message
 
 sleepTime = 2
 count = 0
 
 while True:
    message = 'Hello World! %s'% count
-#   channel.basic_publish(exchange='', routing_key='hello', body=message)  #for single consumer of message 
-   channel.basic_publish(exchange='logs', routing_key='', body=message)   # for multiple consumer of same message
+   channel.basic_publish(exchange='', routing_key=message, body=message)  #for single consumer of message 
+#   channel.basic_publish(exchange='logs', routing_key='', body=message)   # for multiple consumer of same message
 
    count+=1	
    print(" [x] Sent 'Hello World!'")
