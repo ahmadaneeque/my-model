60e4062a2e77cd7477a661fe5b6aea44045254e1
ahmad <ahmadaneequekhalid@gmail.com>
2020-10-06 15:45:12 +0500

    reverting back to single que listner

--- consumer.py
+++ consumer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import pika, sys, os
 
-rabbit_host = os.getenv("RABBIT_HOST",'rabbit')
-rabbit_port = os.getenv("RABBIT_PORT", 5672) 
-rabbit_user = os.getenv("RABBIT_USERNAME", 'guest')
-rabbit_password = os.getenv("RABBIT_PASSWORD", 'guest')  
+rabbit_host = os.getenv("RABBIT_HOST")
+rabbit_port = os.getenv("RABBIT_PORT") 
+rabbit_user = os.getenv("RABBIT_USERNAME")
+rabbit_password = os.getenv("RABBIT_PASSWORD")  
 
 
 def main():
@@ -12,23 +12,21 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_host, port=rabbit_port, credentials=credentials))
 
     channel = connection.channel()
+    channel.queue_declare(queue='hello') # for single message consumer
 
-    channel.exchange_declare(exchange='logs', exchange_type='fanout')    # for multiple consumer of same message
+   # channel.exchange_declare(exchange='logs', exchange_type='fanout')    # for multiple consumer of same message
 
-    result = channel.queue_declare(queue='', exclusive=True)   # create a queue exclusively for this consumer
-  #  channel.queue_declare(queue='hello') # for single message consumer
-
-    queue_name = result.method.queue
-
-    channel.queue_bind(exchange='logs', queue=queue_name)
+  #  result = channel.queue_declare(queue='', exclusive=True)   # create a queue exclusively for this consumer
+    # queue_name = result.method.queue
+    #channel.queue_bind(exchange='logs', queue=queue_name)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
-  #  channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True) 
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True) 
+  #  channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
     channel.start_consuming()
 
