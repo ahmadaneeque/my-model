eaec78fc741e9f7d0d28779a20cfa60133fdb21c
ahmad <ahmadaneequekhalid@gmail.com>
2020-10-07 16:57:16 +0500

    Revert "added example from tutorial 5 of rabbitmq publisher"
    This reverts commit a3480e072654faec91bb12457516e1b144c59bc6.

--- consumer.py
+++ consumer.py
@@ -2,6 +2,13 @@
 import pika, sys, os
 
 
+def main():
+    
+#!/usr/bin/env python
+import pika
+import sys
+
+
 rabbit_host = os.getenv("RABBIT_HOST")
 rabbit_port = os.getenv("RABBIT_PORT") 
 rabbit_user = os.getenv("RABBIT_USERNAME")
