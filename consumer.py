262c4bc40a755298ef752a59260a55949fa3608b
ahmad <ahmadaneequekhalid@gmail.com>
2020-10-22 17:16:21 +0500

    added eviroment variables

--- consumer.py
+++ consumer.py
@@ -28,8 +28,8 @@ channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
 result = channel.queue_declare('', exclusive=True)
 queue_name = result.method.queue
 
-#binding_keys = sys.argv[1:]
-binding_keys = ["topic2"]
+# binding_keys = sys.argv[1:]
+binding_keys = ["kern.*"]
 if not binding_keys:
     sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
     sys.exit(1)
